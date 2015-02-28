634977f430be699802f7ce91ddbab1a974cd53fe
Eric Leung <leunge@ohsu.edu>
2015-02-28 02:23:40 -0800

    add website link

--- inProgress/sharedMotif.py
+++ inProgress/sharedMotif.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 # Finding a Shared Motif
+# http://rosalind.info/problems/lcsm/
 # GIVEN: A collection of k (k <= 100) DNA strings of length at most 1 kbp each
 #   in FASTA format
 # RETURN: A longest common substring of the collection
