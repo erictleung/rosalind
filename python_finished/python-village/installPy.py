02bc4a441c3cbf87721958caabe9be8d49576baa
Eric Leung <etleung21@gmail.com>
2015-06-07 15:30:04 -0700

    rewrite Zen of Python with shell script

--- python_finished/python-village/installPy.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python
-
-# installPy.py
-# Install Python and type "import this" and submit output
-
-import this
---
+++ python_finished/python-village/install_python.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+# INI1 - Installing Python
+
+# GNU bash, version 3.2.57(1)-release (x86_64-apple-darwin14)
+# Copyright (C) 2007 Free Software Foundation, Inc.
+
+# Python 2.7.9 :: Anaconda 2.1.0 (x86_64)
+
+python -c "import this" >> python_zen.txt
