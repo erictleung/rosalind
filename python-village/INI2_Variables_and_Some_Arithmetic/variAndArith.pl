17ae0d273272a4f484584d6699075d43c4e78932
Eric Leung <etleung21@gmail.com>
2015-09-15 22:56:34 -0700

    add date and version comments

--- python-village/INI2_Variables_and_Some_Arithmetic/variAndArith.pl
+++ python-village/INI2_Variables_and_Some_Arithmetic/variAndArith.pl
@@ -4,6 +4,8 @@
 # Given: Two positive integers a and b, each less than 1000
 # Return: The integer corresponding to the square of the hypotenuse of
 #     the right triangle whose legs have lengths a and b
+# Date: 2015 Sept 15
+# Version: 5.16
 
 use strict;
 use warnings;
