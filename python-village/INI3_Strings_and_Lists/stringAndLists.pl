0a5abcabc53e70ce2ed5c66b97d293ec93e0991b
Eric Leung <etleung21@gmail.com>
2015-09-18 12:24:37 -0700

    add comments and output answer to file to INI3

--- python-village/INI3_Strings_and_Lists/stringAndLists.pl
+++ python-village/INI3_Strings_and_Lists/stringAndLists.pl
@@ -5,23 +5,30 @@
 #     a, b, c, d
 # Return: The slice of this string from indices a through b and c through
 #     d (with space in between), inclusively
+# Date: 2015 Sept 18
+# Version: 5.16
+
 use strict;
 use warnings;
 
 open( IN1, $ARGV[0] ) || die "input error.\n";
 
-my $message = <IN1>;
-chomp $message;
-my @str2 = split " ", <IN1>;
+my $message = <IN1>; # first line is one string
+chomp $message; # remove newline character
+my @str2 = split " ", <IN1>; # second line has numbers to be split by spaces
 
+close IN1;
+
+# find lengths of strings to extract in next step
 my $one = $str2[1] - $str2[0] + 1; # Plus one for 1 based numbering
 my $two = $str2[3] - $str2[2] + 1; # for lists
 
-# substr STRING, BEGIN, LENGTH
-
+# find actual words between indices indicated in input file
 my $word1 = substr $message, $str2[0], $one;
 my $word2 = substr $message, $str2[2], $two;
+# substr STRING, BEGIN, LENGTH
 
-print "$word1 $word2\n";
-
-close IN1;
+# save answer in file
+open( MYFILE, '> out.txt' );
+print MYFILE "$word1 $word2";
+close( MYFILE );
