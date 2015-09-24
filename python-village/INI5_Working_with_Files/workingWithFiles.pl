60d1399390b09a051cdee3ec9fb2256bf1358c61
Eric Leung <etleung21@gmail.com>
2015-09-23 21:55:05 -0700

    update working with files with saving answer to file

--- python-village/INI5_Working_with_Files/workingWithFiles.pl
+++ python-village/INI5_Working_with_Files/workingWithFiles.pl
@@ -4,20 +4,24 @@
 # Given: A file containing at most 1000 lines
 # Return: A file containing all the even-numbered lines from the 
 #     original file. Assume 1-based numbering of lines
+# Date: 2015 Sept 23
+# Version: 5.16
 
 use strict;
 use warnings;
 
 open ( IN1, $ARGV[0] ) || die "input error.\n";
+open ( MYFILE, '> out.txt' );
 
 my $n = 1; # First count
 
 while ( <IN1> ) {
     chomp;
     if ( $n % 2 == 0 ) { # If even numbered
-        print "$_\n";
+        print MYFILE "$_\n";
     }
     $n += 1; # Increment
 }
 
 close IN1;
+close MYFILE;
