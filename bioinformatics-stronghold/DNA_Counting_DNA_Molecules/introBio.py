743b2bfdb4f1d9966406edcc9b0860c7e32f94a8
Eric Leung <etleung21@gmail.com>
2015-10-12 07:20:17 -0700

    update nucleotide counting to auto write output file

--- bioinformatics-stronghold/DNA_Counting_DNA_Molecules/introBio.py
+++ bioinformatics-stronghold/DNA_Counting_DNA_Molecules/introBio.py
@@ -3,10 +3,12 @@
 # introBio.py
 # Given: A DNA string s of length at most 1000 bp
 # Return: Four integers (sparated by spaces) representing the respective number
-# of times that the symbol 'A', 'C', 'G', and 'T' occur in S.
+#     of times that the symbol 'A', 'C', 'G', and 'T' occur in S.
+# Usage: python introBio.py FILE
+# Date: 2015 Oct 12
+# Version: 2.7.8
 
 # import necessary packages
-from Bio.Seq import Seq
 import sys
 
 # save file into file
@@ -18,6 +20,7 @@ numC = 0
 numT = 0
 numG = 0
 
+# iterate through each line and count respective letter
 with open(file, "r") as fh:
     for lines in fh:
         numA += lines.count("A")
@@ -25,4 +28,6 @@ with open(file, "r") as fh:
         numT += lines.count("T")
         numG += lines.count("G")
 
-print str(numA) + " " + str(numC) + " " + str(numG) + " " + str(numT)
+# write out answer to file
+with open("out.txt", "a") as out:
+    out.write(str(numA) + " " + str(numC) + " " + str(numG) + " " + str(numT))
