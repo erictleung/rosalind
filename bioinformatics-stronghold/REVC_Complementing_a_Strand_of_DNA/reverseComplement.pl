413a3adf77066d058c9c834d7a5ebeca95a2d161
Eric Leung <etleung21@gmail.com>
2015-10-16 23:30:26 -0700

    update Perl reverse complement script to auto output

--- bioinformatics-stronghold/REVC_Complementing_a_Strand_of_DNA/reverseComplement.pl
+++ bioinformatics-stronghold/REVC_Complementing_a_Strand_of_DNA/reverseComplement.pl
@@ -1,16 +1,21 @@
 #!/usr/bin/perl
 
+# Program: 'reverseComplement'
+# Given: A DNA string s of length at most 1000 bp
+# Return: The reverse complement of s.
+# Date 2015 Oct 16
+# Version: 5.16
+
 use strict;
 use warnings;
 
 open ( IN1, $ARGV[0] ) || die "input error\n";
 
-# Given: A DNA string s of length at most 1000 bp
-# Return: The reverse complement of s.
-
+# split DNA string into array
 my @string = split "", <IN1>;
 my @new;
 
+# loop through string and build new string based on forward string
 foreach ( @string ) {
     if ( $_ eq 'A' ) {
         unshift @new, 'T';
@@ -26,6 +31,8 @@ foreach ( @string ) {
     }
 }
 
-print @new;
+# write output to file
+open ( MYFILE, '> out.txt' );
+print MYFILE @new;
 
 close IN1;
