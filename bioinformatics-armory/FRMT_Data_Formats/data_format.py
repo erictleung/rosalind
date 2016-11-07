3e442a343dbbc4147b8b7b6270e28f06621a52ed
Eric Leung <eric@erictleung.com>
2016-11-06 22:45:25 -0800

    FRMT: query Genbank and return records

--- bioinformatics-armory/FRMT_Data_Formats/data_format.py
+++ bioinformatics-armory/FRMT_Data_Formats/data_format.py
@@ -4,6 +4,8 @@
 
 # import package
 import sys
+from Bio import Entrez
+from Bio import SeqIO
 
 # import file
 data = sys.argv[1]
@@ -12,6 +14,11 @@ data = sys.argv[1]
 with open(data, "r") as fh:
     raw = fh.readline().rstrip()
     ids = raw.split(" ")
+
+# get user information to query
+Entrez.email = raw_input("What is your email?: ")
+handle = Entrez.efetch(db="nucleotide", id=ids, rettype="fasta")
+records = list(SeqIO.parse(handle, "fasta"))
 
 # write output
 with open("out.txt", "a") as out:
