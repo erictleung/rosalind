087c14452fe9dd6400b3ce959854a38b2bd08234
Eric Leung <eric@erictleung.com>
2016-11-15 16:55:04 -0800

    PROT: add comments to code and add sample inputs

--- bioinformatics-stronghold/PROT_Translating_RNA_into_Protein/translationToProtein.pl
+++ bioinformatics-stronghold/PROT_Translating_RNA_into_Protein/translationToProtein.pl
@@ -1,14 +1,16 @@
-#!/usr/bin/perl
+#!/usr/bin/env perl
 
 use strict;
 use warnings;
 
 open( IN1, $ARGV[0] ) || die "input error\n";
 
+# split input string into an array
 my @seq = split "", <IN1>;
 
 close IN1;
 
+# hash lookup table for protein translation
 my %hash = (
     'UUU' => 'F',  'UCU' => 'S',  'UAU' => 'Y',    'UGU' => 'C',
     'UUC' => 'F',  'UCC' => 'S',  'UAC' => 'Y',    'UGC' => 'C',
@@ -28,17 +30,23 @@ my %hash = (
     'GUG' => 'V',  'GCG' => 'A',  'GAG' => 'E',    'GGG' => 'G'
 );
 
+# temporary codon
 my $tri = "";
 
+# loop through array
 while (@seq) {
+    # take every three nucleotides into temporary string
     $tri .= shift @seq;
     $tri .= shift @seq;
     $tri .= shift @seq;
+
+    # stop translation if you hit stop codon
     if ($tri eq 'UAA' or $tri eq 'UAG' or $tri eq 'UGA' ) {
         last;
     }
+
     print "$hash{$tri}";
-    $tri = ""; 
+    $tri = ""; # reset temporary string
 }
 
 print "\n";
